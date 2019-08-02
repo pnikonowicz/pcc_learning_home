f4afec0eaf6a3831c8c143abb2e54076872d1527
pnikonowicz <paul.nikonowicz@gmail.com>
2019-08-02 12:01:21 -0400

    add html

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const http = require('http');
+const http = require('http')
+const fs = require('fs')
 
-const hostname = process.env.VCAP_APP_HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
+const createServer = (err, html)=> {
+    if (err) throw err
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(html)
+    })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    const hostname = process.env.VCAP_APP_HOST || '127.0.0.1'
+    const port = process.env.PORT || 3000
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`)
+    })
+}
+
+fs.readFile('app.html', createServer)
 
