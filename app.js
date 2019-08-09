db31755e94cfba2d2f28b9cecb4b5edf938a43f5
Paul Nikonowicz <pnikonowicz@pivotal.io>
2019-08-09 17:12:28 -0400

    implement dino templating
    and some other stuff
    
    Co-authored-by: Paul Nikonowicz <pnikonowicz@pivotal.io>
    Co-authored-by: David Jahn <djahn@pivotal.io>

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const fs = require('fs')
 const createServer = (err, html)=> {
     if (err) throw err
 
+    html = html.toString().replace(/🦖name_service_url/g, process.env.NAME_SERVICE_URL || 'http://localhost:8080');
     const server = http.createServer((req, res) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'text/html; charset=utf-8')
         res.end(html)
     })
 
-    const hostname = process.env.VCAP_APP_HOST || '127.0.0.1'
+    const hostname = process.env.VCAP_APP_HOST || 'localhost'
     const port = process.env.PORT || 3000
     server.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`)
@@ -18,4 +19,3 @@ const createServer = (err, html)=> {
 }
 
 fs.readFile('app.html', createServer)
-
