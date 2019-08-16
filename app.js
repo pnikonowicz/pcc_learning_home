4afb2873f619890b14907c35632195a65468a391
pnikonowicz <paul.nikonowicz@gmail.com>
2019-08-16 11:36:34 -0400

    using express
    it honestly is a lot easier

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const http = require('http')
 const fs = require('fs')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const createServer = (err, html)=> {
-    if (err) throw err
+app.get('/', function (req, res) {
+    fs.readFile('app.html', (err, html) => {
+        if (err) throw err
 
-    html = html.toString().replace(/🦖name_service_url/g, process.env.NAME_SERVICE_URL || 'http://localhost:8080');
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html; charset=utf-8')
-        res.end(html)
+        let nameUrl = process.env.NAME_SERVICE_URL || 'http://localhost:8080';
+        const expanded_html = html.toString().replace(/🦖name_service_url/g, nameUrl);
+        res.send(expanded_html)
     })
+})
 
-    const hostname = process.env.VCAP_APP_HOST || 'localhost'
-    const port = process.env.PORT || 3000
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`)
-    })
-}
-
-fs.readFile('app.html', createServer)
+app.listen(PORT,function(){
+    console.log('Server is running at PORT:',PORT);
+});
