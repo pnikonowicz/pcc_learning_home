7590151e8e7908addb1e84cfbda341320cda4772
pnikonowicz <paul.nikonowicz@gmail.com>
2019-09-13 12:20:00 -0400

    integrate answer service

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ app.get('/', function (req, res) {
     fs.readFile('app.html', (err, html) => {
         if (err) throw err
 
-        let nameUrl = process.env.NAME_SERVICE_URL || 'http://localhost:8080';
-        let userStatusUrl = process.env.USER_STATUS_SERVICE_URL || 'http://localhost:8080';
+        const nameUrl = process.env.NAME_SERVICE_URL || 'http://localhost:8080'
+        const userStatusUrl = process.env.USER_STATUS_SERVICE_URL || 'http://localhost:8080'
+        const answerUrl = process.env.ANSWER_SERVICE_URL || 'http://localhost:3001'
+
         const expanded_html = html.toString()
             .replace(/🦖name_service_url/g, nameUrl)
-            .replace(/🦖user_status_service_url/g, userStatusUrl);
+            .replace(/🦖user_status_service_url/g, userStatusUrl)
+            .replace(/🦖answer_service_url/g, answerUrl)
         res.send(expanded_html)
     })
 })
 
 app.listen(PORT,function(){
-    console.log('Server is running at PORT:',PORT);
-});
+    console.log('Server is running at PORT:',PORT)
+})
